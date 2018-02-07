133be10ecef917f9e6018671f440467c868dc747
Mac Cooper <macooper@csumb.edu>
2018-02-07 17:33:48 +0000

    i modified the index.html

--- index.php
+++ index.php
@@ -4,6 +4,15 @@
         <title> </title>
     </head>
     <body>
+        <div>
+            <!--start at hw-->
+            <h1>hw</h1>
+            <!--look for hw1-->
+            <a href="/hw/Homework1/index.html"></a>
+            
+            <h1>labs</h1>
+            <ahref="/labs/personalportfolio/index.html"></a>
+        </div>
 
     </body>
 </html>
