fce898d2b3588ba5276e595dc499384df6413511
Mac Cooper <macooper@csumb.edu>
2018-02-19 17:28:31 +0000

    add a new text

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     </head>
     <body>
         <div>
+            
             <!--start at hw-->
             <h1>hw</h1>
             <!--look for hw1-->
             <a href="/hw/Homework1/index.html"></a>
             
             <h1>labs</h1>
-            <ahref="/labs/personalportfolio/index.html"></a>
+            <a href="/labs/personalportfolio/index.html"></a>
         </div>
-
+hey how are you today
     </body>
 </html>
