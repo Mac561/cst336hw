f26c15df0bb8d17ad1a219f8449b5d7695bc6eb0
Mac Cooper <macooper@csumb.edu>
2018-02-20 01:11:25 +0000

    cst336 homework slot machain game and it play a sound when the user get all three sysmbols in a row

---
+++ labs/777/inc/functions.php
@@ -0,0 +1,70 @@
+<?php
+
+ function play(){
+           
+           for ($i=1;$i<4;$i++){
+            ${"randomValue".$i} = rand(0,2);
+            displaySymbol(${"randomValue" .$i}, $i);
+        }
+        displayPoints($randomValue1, $randomValue2, $randomValue3);
+        
+       }
+        
+        function displaySymbol($randomValue, $pos){
+            
+            /*if ($randomValue==0){
+                echo ' <img src="img/seven.png" alt="seven" title="Seven" width= 70 />';
+            }else if ($randomValue==1){
+                echo ' <img src="img/cherry.png" alt="cherry" title="Cherry" width= 70 />';
+            }else{
+                echo ' <img src="img/lemon.png" alt="lemon" title="Lemon" width= 70 />';
+            }*/
+            
+            switch($randomValue){
+                case 0: $symbol = "seven";
+                    break;
+                case 1: $symbol = "cherry";
+                    break;
+                case 2: $symbol = "grapes";
+                    break;
+            }
+            echo "<img id='reel$pos' src='img/$symbol.png' alt='$symbol' title='".ucfirst($symbol)."' width='70' >";
+        }
+    ////////////////////////////////////////////////////////////
+    
+     function displayPoints($randomValue1,$randomValue2,$randomValue3)
+        {
+            echo "<div id='output'>";
+            if ($randomValue1 == $randomValue2 && $randomValue2 == $randomValue3)
+            {
+                switch($randomValue1)
+                {
+                    case 0:$totalPoints = 1000;
+                           echo "<h1>jackpot!</h1>";
+                           echo "<audio controls autoplay>
+                                 <source src='jps/bcs.mp3' type='audio/ogg'>
+                                 Your browser does not support the audio element.
+                                </audio>";
+                          //echo "<audio src='jps/bcs.mp3' hidden= 'true' autostart= 'true' loops= 'true' />";
+                        break;
+                    case 2: $totalPoints = 900;
+                       echo "<audio controls autoplay>
+                             <source src='jps/bcs.mp3' type='audio/ogg'>
+                            Your browser does not support the audio element.
+                            </audio>"; break;
+                    case 3:$totalPoints =250;
+                       echo "<audio controls autoplay>
+                            <source src='jps/bcs.mp3' type='audio/ogg'>
+                            Your browser does not support the audio element.
+                            </audio>";
+                            break;
+                }
+                
+                echo " <h2> You won $totalPoints </h2>";
+            }else {
+                echo " <h3> Try Again </h3>";
+                  }
+                echo " </div>";
+        }
+
+?>
